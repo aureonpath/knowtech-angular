d00d342b5e32d13200c8304fd3d68b26f1e72f99
ERNI\coju <juancarlos.coello@erni-espana.es>
2016-12-01 18:17:34 +0100

    reset files

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = webpackMerge(commonConfig, {
 
   output: {
     path: helpers.root('dist'),
-    publicPath: '/',
+    publicPath: '/dist/',
     filename: '[name].[hash].js',
     chunkFilename: '[id].[hash].chunk.js'
   },
