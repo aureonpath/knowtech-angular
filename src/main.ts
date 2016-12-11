cf325f1936b0fd583be5115d0040643b1ae29076
aureonpath <aureonpath@gmail.com>
2016-12-11 17:38:04 +0100

    bas component

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import '../public/css/styles.css';
 
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+@Component({
+  selector: 'my-app',
+  template: `
+        <div class="container">
+        <h1>First Component</h1>
+        </div>
+  `
+})
+class AppComponent {
+  title = 'First Component';
+ }
+
+@NgModule({
+  imports: [
+    BrowserModule
+  ],
+  declarations: [ AppComponent ],//all the components inside the module
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+class AppModule { }
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .then(success => console.log(`Bootstrap success`))
