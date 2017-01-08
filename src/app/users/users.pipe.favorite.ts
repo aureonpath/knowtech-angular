96b70fe6f6be06a9d0b7eddf024df09bb02d86ec
aureonpath <aureonpath@gmail.com>
2017-01-08 20:32:52 +0100

    pure flag

--- src/app/users/users.pipe.favorite.ts
+++ src/app/users/users.pipe.favorite.ts
@@ -2,7 +2,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 import { User } from './user';
 
-@Pipe({ name: 'favoritedUsers' })
+@Pipe({ name: 'favoritedUsers', pure: false })
 export class FavoritedUsersPipe implements PipeTransform {
   transform(allUsers: User[], isActive: boolean) {
     return isActive ? allUsers.filter(user => user.isFavorited) : allUsers;
